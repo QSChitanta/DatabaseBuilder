cf56f7ff149c68847eeacb406c2fccf4ccaea1e4
QSChitanta <patrick.chitanta@quinscape.de>
2022-06-15 17:01:27 +0200

    refactored and documented metods

--- src/de/quinscape/Database/DatabaseManager.java
+++ src/de/quinscape/Database/DatabaseManager.java
@@ -4,47 +4,86 @@ import de.quinscape.FileInputConverter.ArrayListToObjectFileConverter;
 import de.quinscape.FileOutputConverter.ObjectFileToArrayListConverter;
 import de.quinscape.Model.Client;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+/**
+ * This class manages the database.
+ * It creates or serializes, deserializes and saves a database file.
+ *
+ */
 public class DatabaseManager {
-    static ArrayList<Client> importedClientList = new ArrayList<>();
+    private ArrayList<Client> importedClientList = new ArrayList<>();
+    private final File databaseFile = new File("Client_Database.dat");
+    private Client client = new Client();
+
     public void runProgram() throws IOException, ClassNotFoundException {
-        ArrayListToObjectFileConverter clientList = new ArrayListToObjectFileConverter();
-        ObjectFileToArrayListConverter objectToArrayList = new ObjectFileToArrayListConverter();
-        Client testClient = new Client();
-
-
-        //Initializes ArrayList<Client> and converts it to an .dat File
-        clientList.convertArrayListToFile();
-
-        //Reads .dat File with the saved ArrayList
-        importedClientList = objectToArrayList.importClientArrayListFile();
-
-        //Uses the .dat file as an ArrayList to append new Clients
-        appendNewClient(testClient);
-            //how do i close the ObjectFileToArrayListConverter - The OutputStreams are not closed
-
-        //TODO: Saving/Overwriting loaded .dat File / Datahase file with the "new" ArrayList
-        //FIXME: -> convert into File again and overwrite Client_Database.dat
-
+        createOrReadFile();
+        printContent();
     }
-    
-    //Test data
+
     /**
-     * appends Client to Array
-     * @param testClient
+     * Serializes an ArrayList to a "Client_Database.dat" file. If that file exists:
+     * 1) deserialize / open the file; 2) append new clients to its list,3) serialize, close and save the file
+     * @throws IOException
+     * @throws ClassNotFoundException
      */
-    private void appendNewClient(Client testClient) {
-        testClient.setName("Patrick");
-        testClient.setInsuranceNumber(1234);
-        importedClientList.add(testClient);
-        printContent(importedClientList);
-        System.out.println(importedClientList);
+    private void createOrReadFile() throws IOException, ClassNotFoundException {
+        ArrayListToObjectFileConverter clientList = new ArrayListToObjectFileConverter();
+        ObjectFileToArrayListConverter objectFileToArrayListConverter = new ObjectFileToArrayListConverter();
+
+        boolean doesFileExist = objectFileToArrayListConverter.databaseFile().exists();
+        if (!doesFileExist){
+            //Initializes ArrayList<Client> and converts it to an .dat File
+            clientList.convertArrayListToFile();
+        } else {
+            //opens database as an ArrayList Object
+            deserializeClientList(objectFileToArrayListConverter);
+            //append new clients to the now opened list
+            appendNewClientToList();
+            //save the list as an object inside a file and close the file.
+            serializeClientList();
+        }
     }
 
-    private void printContent(ArrayList<Client> importedClientList) {
-        System.out.println(importedClientList.get(0).getInsuranceNumber());
-        System.out.println(importedClientList.get(0).getName());
+    /**
+     * creates a new Client object and adds it to the list of clients
+     */
+    private void appendNewClientToList() {
+        client.setInsuranceNumber(44147);
+        client.setName("Thor");
+        importedClientList.add(client);
+    }
+
+    /**
+     * opens the saved ArrayList from the File a
+     * @param objectFileToArrayListConverter
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private void deserializeClientList(ObjectFileToArrayListConverter objectFileToArrayListConverter) throws IOException, ClassNotFoundException {
+        importedClientList = objectFileToArrayListConverter.importClientArrayListFile();
+    }
+
+    /**
+     * saves the importedClientList as a .dat file
+     * @throws IOException
+     */
+    private void serializeClientList() throws IOException {
+        FileOutputStream file = new FileOutputStream(databaseFile);
+        ObjectOutputStream newFile = new ObjectOutputStream(file);
+        newFile.writeObject(importedClientList);
+        newFile.close();
+        file.close();
+    }
+
+    /**
+     * prints every element from list
+     */
+    private void printContent() {
+        System.out.println(importedClientList);
     }
 }
